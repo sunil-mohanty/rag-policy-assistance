c7f2b4a1df0412afa974155a9e5938b8337b1dfa
sunil.mohanty@live.com <sunil.mohanty@live.com>
2026-08-08 21:20:12 +0400

    updated the runpod start up script

--- start-runpod.sh
+++ start-runpod.sh
@@ -38,7 +38,7 @@ cd /workspace/hr-rag-chatbot
 if [ ! -d "venv" ]; then
     python3 -m venv venv
     source venv/bin/activate
-    pip install -r ../requirements.txt
+    pip install -r requirements.txt
 else
     source venv/bin/activate
 fi
